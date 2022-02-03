2e06c8765a6a72ca24e82cc96337c437a9d0b570
EuneeChung <chungee375@gmail.com>
2022-02-03 12:22:26 +0900

    feat: 🎸 부모 HOME - 아이 정보 카드 컴포넌트로 분리

--- frontend/src/views/parent/Home.vue
+++ frontend/src/views/parent/Home.vue
@@ -1,69 +1,16 @@
 <template>
   <div  class="ma-4">
-    <h1>
-      <p class="font-weight-black">아이 정보</p>
-    </h1>
+    <h1 class="font-weight-black">아이 정보</h1>
     <v-sheet class="mx-auto" elevation="" max-width="1200">
       <v-slide-group v-model="model" class="pa-1" center-active show-arrows>
         <v-slide-item
           justify-center
-          v-for="n in 5"
-          :key="n"
+          v-for="(card, i) in items"
+          :key="i"
           v-slot="{ active, toggle }"
         >
-          <v-card
-            elevation="4"
-            max-width="250"
-            outlined
-            :color="active ? 'parent02' : 'white'"
-            class="ma-4 justify-center"
-            height="200"
-            width="150"
-            @click="toggle"
-          >
-            <div class="text-center mb-1">
-              <v-list-item two-line>
-                <v-list-item-content class="pa-2 mb-1">
-                  <!-- <div class="text-overline mb-4 justify-center">OVERLINE</div> -->
-                  <v-list-item-title class="font-weight-black text-h5 justify-start ma-2 pa-0">
-                    정은이
-                  </v-list-item-title>
-                  <v-list-item-subtitle class="justify-start ma-0 pa-0"
-                    >14,000단추</v-list-item-subtitle
-                  >
-                </v-list-item-content>
-              </v-list-item>
-              <v-progress-circular
-                align="center"
-                :rotate="-90"
-                :size="100"
-                :width="20"
-                :value="value"
-                color="parent01"
-                class="justify-center mb-4"
-              >
-                {{ progressAmount }}
-                완료
-              </v-progress-circular>
-            </div>
-            <!-- </v-card> -->
-            <v-row
-              class="fill-height justify-center"
-              align="center"
-              justify="center"
-            >
-              <v-flex justify-center>
-                <v-scale-transition>
-                  <v-icon
-                    v-if="active"
-                    color="white"
-                    size="48"
-                    v-text="'mdi-close-circle-outline'"
-                  ></v-icon>
-                </v-scale-transition>
-              </v-flex>
-            </v-row>
-          </v-card>
+        <child-card-info @click="toggle"  :item="card"  :color="active ? 'parent02' : 'white'">
+        </child-card-info>
         </v-slide-item>
       </v-slide-group>
     </v-sheet>
@@ -72,7 +19,9 @@
 </template>
 
 <script>
+import ChildCardInfo from './ChildCardInfo.vue';
 export default {
+  components: { ChildCardInfo },
   name: "Home",
   data() {
     return {
@@ -81,16 +30,16 @@ export default {
       progressAmount: "2/3",
       items: [
         {
-          color: "#1F7087",
-          src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-          title: "Supermodel",
-          artist: "Foster the People",
+          name: "정은이",
+          danchuAmount: "32,000 단추",
+          progressValue: 30,
+          progressAmount: "1/3",
         },
         {
-          color: "#952175",
-          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
-          title: "Halcyon Days",
-          artist: "Ellie Goulding",
+          name: "김응철",
+          danchuAmount: "14,000 단추",
+          progressValue: 60,
+          progressAmount: "2/3",
         },
       ],
     };
