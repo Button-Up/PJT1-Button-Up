3b55bc14b2a1cb45a1c1f0bf2f061afbeddc53e3
EuneeChung <chungee375@gmail.com>
2022-02-04 20:46:44 +0900

    feat: 🎸 부모 홈 - 아이 정보 - 단추 계좌 컴포넌트
    ✅ Closes: #S06P12B205-180

---
+++ frontend/src/components/parent/home/ChildAccount.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card
+    class="mr-auto ma-0"
+    outlined
+    rounded="xl"
+    elevation="4"
+    :color="account.isDeposit ? 'parent01': 'parent03'"
+  >
+    <v-list-item three-line class="text-center justify-center pa-0">
+      <v-list-item-content class="pa-2">
+        
+        <v-card-text v-text="savingType" :class="account.isDeposit ? 'white--text' : 'black--text'" class="pt-3 pb-0"></v-card-text>
+        <v-list-item-title class="text-h4 ma-1 font-weight-bold " :class="account.isDeposit ? 'white--text' : 'black--text'">
+          {{account.amount}}
+        </v-list-item-title>
+        <v-card-text v-text="`단추`" :class="account.isDeposit ? 'white--text' : 'black--text'" class="pb-3 pt-2"></v-card-text>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ChildAccount',
+  data(){
+    return{ savingType:`적금`}
+  },
+  props:{
+    account:Object
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/parent/home/ChildInfo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="mx-6 mt-6">
+    <!-- 아이의 단추 계좌 START -->
+    <div>
+      <v-row id="space-between" class="ma-0 align-center"> 
+        <h2>아이의 단추 계좌</h2>
+        <v-btn small color="parent01" class="white--text">입금하기</v-btn>
+      </v-row>
+      <v-row >
+        <v-col v-for="(account, a) in accounts"
+            :key="a" class="mt-2 pa-1 align-center"> 
+        <child-account :account="account"></child-account>
+        </v-col>
+      </v-row>
+      <v-list-item>
+
+      </v-list-item>
+    </div> 
+    <!-- 아이의 단추 계좌 END -->
+    <div>
+      <h2>아이의 할 일 진행 상황</h2>
+      <job-card> </job-card>
+    </div> 
+  </div>
+</template>
+
+<script>
+import JobCard from '../../common/JobCard.vue'
+import ChildAccount from './ChildAccount.vue'
+export default {
+  name: 'ChildInfo',
+  components: { JobCard, ChildAccount },
+  data(){
+    return{
+      accounts:[
+        {
+          isDeposit:true,
+          amount:"25,000"
+        },
+        {
+          isDeposit:false,
+          amount:"15,000"
+        },
+      ],
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+#space-between{
+  justify-content: space-between
+}
+</style>
