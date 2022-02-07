03c707f64cd1bbc91c3b21e48b4eb49cb3cc1035
EuneeChung <chungee375@gmail.com>
2022-02-08 01:15:48 +0900

    feat: 🎸 부모 직업관리 - 직업 종류 조회
    ✅ Closes: #S06P12B205-212

--- frontend/src/components/parent/activity/JobManage.vue
+++ frontend/src/components/parent/activity/JobManage.vue
@@ -5,12 +5,36 @@
       <h2>직업 종류 관리</h2>
       <p class="caption font-weight-normal">직업을 추가하거나, 기존 직업의 역할, 급여를 수정할 수 있어요.</p>
     </div>
+    <v-sheet class="mx-auto" elevation="" max-width="1200">
+        <v-slide-group class="pa-1" center-active>
+          <!--
+            아이 정보 카드 클릭 시 activity로 이동하게 되어있습니다.
+            추후 아이 상세 페이지로 이동하도록 수정 필요
+          -->
+          <v-slide-item
+            justify-center
+            v-for="(job, j) in jobs"
+            :key="j"
+          >
+            <job-with-todo-list-card :isParent="true" :job="job"></job-with-todo-list-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
+
+    <v-btn
+          :to="{ name: 'ParentSignupSyncCodeForm',}"
+          block
+          color="parent01"
+          class="white--text"
+        >직업 추가하기 </v-btn>
     
     <!-- 아이 직업 설정하기 END -->
-    <div>
+    <div class="mt-8">
       <h2>아이 직업 설정하기</h2>
       <p class="caption font-weight-normal">위에서 생성, 수정한 직업을 아이에게 부여해주세요.</p>
     </div> 
+    <div>
+    </div>
     <div>
       <v-divider></v-divider>
       <template v-for="(item, index) in items" class="ma-0 pa-0">
@@ -35,9 +59,12 @@
 </template>
 
 <script>
-
+import JobWithTodoListCard from '../../common/JobWithTodoListCard.vue';
+// import JobCard from './JobCard.vue'
 export default {
   components: { 
+    // JobCard,
+    JobWithTodoListCard
   },
   data() {
     return {
@@ -64,11 +91,62 @@ export default {
       },
       ],
       jobs:[
-        { name: '직업 설정', type: 'FL' },
-        { name: '환경미화원', type: 'GA' },
-        { name: '세탁소', type: 'NE' },
-        { name: '기상청장', type: 'CA' },
-      ]
+        {  
+          name:'청소부',
+          image: 'https://cdn.vuetifyjs.com/images/john.jpg',
+          pay_term:"주급",
+          salary: 3000,
+          TodoList:[
+            {
+              done:false,
+              task:"방 정리 하기",
+            },
+            {
+              done:false,
+              task:"먼지 털기",
+            },
+            {
+              done:false,
+              task:"청소기 돌리기",
+            }
+          ]
+        },
+        {  
+          name:'환경미화원',
+          image: 'https://cdn.vuetifyjs.com/images/john.jpg',
+          salary: 3000,
+          pay_term:"주급",
+          TodoList:[
+            {
+              done:false,
+              task:"고양이 화장실 치우기",
+            },
+            {
+              done:false,
+              task:"음식물 쓰레기 버리기",
+            },
+            {
+              done:false,
+              task:"분리수거 하기",
+            }
+          ]
+        },
+        { 
+          name:'기상청장',
+          image: 'https://cdn.vuetifyjs.com/images/john.jpg',
+          salary: 3000,
+          pay_term:"주급",
+          TodoList:[
+            {
+              done:false,
+              task:"환기 시키기",
+            },
+            {
+              done:false,
+              task:"날씨 알려주기",
+            }
+          ] },
+      ],
     }
   },
   methods:{
