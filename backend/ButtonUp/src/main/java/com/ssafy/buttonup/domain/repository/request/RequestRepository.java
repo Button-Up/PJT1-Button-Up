53f03fc0c12e14f5974e68b1b3bdbf247957a0bc
WooJeongYeon <wjygogogo@gmail.com>
2022-02-07 17:39:14 +0900

    feat: 🎸 merge

--- backend/ButtonUp/src/main/java/com/ssafy/buttonup/domain/repository/request/RequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.buttonup.domain.repository.request;
-
-import com.ssafy.buttonup.domain.model.entity.request.RequestHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestRepository extends JpaRepository<RequestHistory, Long> {
-}
