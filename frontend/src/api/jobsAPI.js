b9b260386a2ac6c2e9fc9744bade222cb783b839
김응철 <zbqlr456@naver.com>
2022-02-08 14:56:51 +0900

    refactor: 💡 api 함수 주석 추가

--- frontend/src/api/jobsAPI.js
+++ frontend/src/api/jobsAPI.js
@@ -6,18 +6,22 @@ import { apiInstance } from "./index.js";
 
 const api = apiInstance();
 
+// 새로운 직업 추가
 function addJob(formData, success, fail) {
   api.post(`/jobs`, JSON.stringify(formData)).then(success).catch(fail);
 }
 
+// 직업 정보 조회
 function getJobDetail(jobSeq, success, fail) {
   api.get(`/jobs/${jobSeq}`).then(success).catch(fail);
 }
 
+// 아이의 현재 직업 정보 조회
 function getChildsJob(jobSeq, success, fail) {
   api.get(`/jobs/children/${jobSeq}`).then(success).catch(fail);
 }
 
+// 아이의 직업 내역 추가
 function modifyJob(formData, success, fail) {
   api
     .post(`/jobs/histories`, JSON.stringify(formData))
@@ -25,6 +29,7 @@ function modifyJob(formData, success, fail) {
     .catch(fail);
 }
 
+// 직업 목록 조회
 function getJobsList(parentSeq, success, fail) {
   api.get(`/jobs/parents/${parentSeq}`).then(success).catch(fail);
 }
