573a7f5a40edb6466384762962fabfbfcd9d67b2
WooJeongYeon <wjygogogo@gmail.com>
2022-02-08 21:59:22 +0900

    docs: ✏️ 백앤드 -요청 내역 swagger description, 주석 추가
    swagger, 주석

--- backend/ButtonUp/src/main/java/com/ssafy/buttonup/domain/service/request/RequestService.java
+++ backend/ButtonUp/src/main/java/com/ssafy/buttonup/domain/service/request/RequestService.java
@@ -20,6 +20,12 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 요청 내역 서비스
+ * @author jeongyeon woo
+ * created on 2022-02-07
+ */
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -28,7 +34,12 @@ public class RequestService {
     public final AccountService accountService;
     public final ChildRepository childRepository;
 
-    @Transactional(rollbackFor = Exception.class)
+    /**
+     * 환전 요청 내역 추가 및 입출금 내역 추가(입금)
+     * @param request
+     * @throws BalanceOverException
+     */
+    @Transactional(rollbackFor = Exception.class)   // 해당 Exception이 발생해도 rollback 진행!(단추를 초과해 환전 요청 한 경우)
     public void insertExchange(ExchangeRequest request) throws BalanceOverException {
         long childSeq = request.getChildSeq();
         Child child = childRepository.getById(childSeq);
@@ -42,7 +53,7 @@ public class RequestService {
                 .price(price)
                 .child(child)
                 .build();
-        requestHistoryRepository.save(requestHistory);
+        requestHistoryRepository.save(requestHistory);      // 요청 내역 추가
 
         AccountHistoryCategory category = AccountHistoryCategory.환전;
         AccountHistoryType accountHistoryType = AccountHistoryType.출금;
@@ -53,16 +64,23 @@ public class RequestService {
                 .money(price)
                 .childSeq(childSeq)
                 .build();
-        accountService.insertAccountHistory(historyRequest, accountHistoryType);
+        accountService.insertAccountHistory(historyRequest, accountHistoryType);       // 입출금 내역 추가(출금)
     }
 
+    /**
+     * 요청 내역 상태 변환
+     * REJECT, APPROVE
+     * @param responseHistorySeq
+     * @param status
+     * @throws BalanceOverException
+     */
     @Transactional
     public void changeStatus(long responseHistorySeq, RequestHistoryStatus status) throws BalanceOverException {
         RequestHistory requestHistory = requestHistoryRepository.getById(responseHistorySeq);
         requestHistory.changeStatus(status);
         requestHistoryRepository.save(requestHistory);
 
-        if(status.equals(RequestHistoryStatus.REJECT)) {
+        if(status.equals(RequestHistoryStatus.REJECT)) {        // 거절인 경우, 입출금 내역에 입금이 추가되어야 함
             AccountHistoryCategory category = AccountHistoryCategory.환전;
             AccountHistoryType accountHistoryType = AccountHistoryType.입금;
             final String content = "환전 거절";
@@ -72,10 +90,15 @@ public class RequestService {
                     .money(requestHistory.getPrice())
                     .childSeq(requestHistory.getChild().getSeq())
                     .build();
-            accountService.insertAccountHistory(historyRequest, accountHistoryType);
+            accountService.insertAccountHistory(historyRequest, accountHistoryType);    // 입출금 내역 추가
         }
     }
 
+    /**
+     * 아이의 요청 내역 목록 조회
+     * @param childSeq
+     * @return
+     */
     public List<RequestHistoryResponse> findByChildSeq(long childSeq) {
         List<RequestHistory> requestHistories = requestHistoryRepository.findByChild_SeqOrderByDateAsc(childSeq);
         List<RequestHistoryResponse> responses = new ArrayList<>();
@@ -92,6 +115,11 @@ public class RequestService {
         return responses;
     }
 
+    /**
+     * 요청 내역 단일 조회
+     * @param requestSeq
+     * @return
+     */
     public RequestHistoryResponse findBySeq(long requestSeq) {
         RequestHistory requestHistory = requestHistoryRepository.getById(requestSeq);
         RequestHistoryResponse response = RequestHistoryResponse.builder()
