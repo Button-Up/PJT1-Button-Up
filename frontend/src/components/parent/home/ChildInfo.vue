680330ec3fd97c5dee50f013ce7f1b78f6e22e01
rlajiun <rlajiun@kakao.com>
2022-02-08 18:05:11 +0900

    feat: 🎸 Add Deposit Vue For Parent

--- frontend/src/components/parent/home/ChildInfo.vue
+++ frontend/src/components/parent/home/ChildInfo.vue
@@ -5,75 +5,87 @@
   <div class="mx-6 mt-6">
     <!-- 아이의 단추 계좌 START -->
     <div>
-      <v-row id="space-between" class="ma-0 align-center"> 
+      <v-row id="space-between" class="ma-0 align-center">
         <h2>아이의 단추 계좌</h2>
-        <v-btn small color="parent01" class="white--text">입금하기</v-btn>
+        <bottom-sheet
+          btnColor="parent01"
+          btnName="입금하기"
+          btnClass="white--text"
+          :isSmallBtn="true"
+        >
+          <template v-slot:body>
+            <deposit></deposit>
+          </template>
+        </bottom-sheet>
       </v-row>
-      <v-row >
-        <v-col v-for="(account, a) in accounts"
-            :key="a" class="mt-2 pa-1 align-center"> 
-        <child-account :account="account"></child-account>
+      <v-row>
+        <v-col v-for="(account, a) in accounts" :key="a" class="mt-2 pa-1 align-center">
+          <child-account :account="account"></child-account>
         </v-col>
       </v-row>
-      <v-list-item>
-
-      </v-list-item>
-    </div> 
+      <v-list-item> </v-list-item>
+    </div>
     <!-- 아이의 단추 계좌 END -->
     <div>
       <h2>아이의 할 일 진행 상황</h2>
-      <job-with-todo-list-card :isParent="true" :job="job" :checkboxOff="false"></job-with-todo-list-card>
-    </div> 
+      <job-with-todo-list-card
+        :isParent="true"
+        :job="job"
+        :checkboxOff="false"
+      ></job-with-todo-list-card>
+    </div>
   </div>
 </template>
 
 <script>
-import JobWithTodoListCard from '../../common/JobWithTodoListCard.vue'
-import ChildAccount from './ChildAccount.vue'
+import BottomSheet from "@/components/common/BottomSheet";
+import JobWithTodoListCard from "../../common/JobWithTodoListCard.vue";
+import ChildAccount from "./ChildAccount.vue";
+import Deposit from "./Deposit.vue";
+
 export default {
-  name: 'ChildInfo',
-  components: { JobWithTodoListCard, ChildAccount },
-  data(){
-    return{
-      accounts:[
+  name: "ChildInfo",
+  components: { BottomSheet, JobWithTodoListCard, ChildAccount, Deposit },
+  data() {
+    return {
+      accounts: [
         {
-          isDeposit:false,
-          amount:"25,000"
+          isDeposit: false,
+          amount: "25,000",
         },
         // {
         //   isDeposit:false,
         //   amount:"15,000"
         // },
       ],
-      job:{
-          name:'청소부',
-          image: 'https://cdn.vuetifyjs.com/images/john.jpg',
-          TodoList:[
+      job: {
+        name: "청소부",
+        image: "https://cdn.vuetifyjs.com/images/john.jpg",
+        TodoList: [
           {
-            done:false,
-            task:"투자 가격 업데이트",
-            url:'/parent/activity',
+            done: false,
+            task: "투자 가격 업데이트",
+            url: "/parent/activity",
           },
           {
-            done:true,
-            task:"투자 가격 업데이트",
-            url:'/parent/userinfo',
+            done: true,
+            task: "투자 가격 업데이트",
+            url: "/parent/userinfo",
           },
           {
-            done:true,
-            task:"투자 가격 업데이트",
-            url:'/parent/userinfo',
-          }
-        ]
-        },
-    }
-  }
-
-}
+            done: true,
+            task: "투자 가격 업데이트",
+            url: "/parent/userinfo",
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
 
 <style scoped>
-#space-between{
-  justify-content: space-between
+#space-between {
+  justify-content: space-between;
 }
 </style>
