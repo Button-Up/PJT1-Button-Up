99b68a6ee79eead81aa49270c2972d3092dd21e4
WooJeongYeon <wjygogogo@gmail.com>
2022-02-09 15:36:50 +0900

    feat: 🎸 프론트 - requestAPI.js 추가

--- frontend/src/views/parent/RequestList.vue
+++ frontend/src/views/parent/RequestList.vue
@@ -1,22 +1,11 @@
 <template>
   <div>
-    <v-list
-      subheader
-      two-line
-    >
+    <v-list subheader two-line>
       <v-subheader>대기 중인 요청</v-subheader>
       <!-- 환전 요청 -->
-      <v-list-item
-        v-for="(exchangeRequest, idx) in activeExchangeRequests"
-        :key="idx"
-      >
+      <v-list-item v-for="(exchangeRequest, idx) in activeExchangeRequests" :key="idx">
         <v-list-item-avatar>
-          <v-icon
-            class="parent01 lighten-1"
-            dark
-          >
-            mdi-hand-coin
-          </v-icon>
+          <v-icon class="parent01 lighten-1" dark> mdi-hand-coin </v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
@@ -27,21 +16,10 @@
         <v-list-item-action>
           <v-list-item-action-text v-text="exchangeRequest.requestedTime"></v-list-item-action-text>
           <!-- 바텀 시트 버튼 -->
-          <bottom-sheet
-            sheetHeight="320px"
-            :isIcon="true"
-            iconName="mdi-arrow-right"
-          >
+          <bottom-sheet sheetHeight="320px" :isIcon="true" iconName="mdi-arrow-right">
             <template v-slot:body>
-              <v-card
-                class="rounded-0"
-                :elevation="0"
-              >
-                <v-toolbar
-                  color="parent01"
-                  dark
-                  :elevation="0"
-                >
+              <v-card class="rounded-0" :elevation="0">
+                <v-toolbar color="parent01" dark :elevation="0">
                   환전 요청 - {{ exchangeRequest.childName }}
                 </v-toolbar>
                 <v-subheader>{{ exchangeRequest.requestedTime }}</v-subheader>
@@ -65,17 +43,9 @@
       </v-list-item>
 
       <!-- 구매 요청 -->
-      <v-list-item
-        v-for="(buyRequest, idx) in activeBuyRequests"
-        :key="idx"
-      >
+      <v-list-item v-for="(buyRequest, idx) in activeBuyRequests" :key="idx">
         <v-list-item-avatar>
-          <v-icon
-            class="parent02 lighten-1"
-            color="black"
-          >
-            mdi-basket
-          </v-icon>
+          <v-icon class="parent02 lighten-1" color="black"> mdi-basket </v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
@@ -85,27 +55,19 @@
 
         <v-list-item-action>
           <v-list-item-action-text v-text="buyRequest.requestedTime"></v-list-item-action-text>
-          <bottom-sheet
-            sheetHeight="260px"
-            :isIcon="true"
-            iconName="mdi-arrow-right"
-          >
+          <bottom-sheet sheetHeight="260px" :isIcon="true" iconName="mdi-arrow-right">
             <template v-slot:body>
-              <v-card
-                class="rounded-0"
-                :elevation="0"
-              >
-                <v-toolbar
-                  color="parent02"
-                  class="font-weight-bold"
-                  :elevation="0"
-                >
+              <v-card class="rounded-0" :elevation="0">
+                <v-toolbar color="parent02" class="font-weight-bold" :elevation="0">
                   구매 요청 - {{ buyRequest.childName }}
                 </v-toolbar>
                 <v-subheader>{{ buyRequest.requestedTime }}</v-subheader>
                 <div class="mx-4">
                   <div class="text-h5 font-weight-bold">{{ buyRequest.productName }}</div>
-                  <div class="text-body-2 my-4">아이가 모아둔 단추로 <br> {{ buyRequest.productName }}를 사고 싶어합니다.</div>
+                  <div class="text-body-2 my-4">
+                    아이가 모아둔 단추로 <br />
+                    {{ buyRequest.productName }}를 사고 싶어합니다.
+                  </div>
                   <v-row dense>
                     <v-col><v-btn block color="parent01" class="white--text">결제하기</v-btn></v-col>
                     <v-col><v-btn block color="parent03">구매 요청 거절</v-btn></v-col>
@@ -122,17 +84,9 @@
       <v-subheader>처리 완료 요청</v-subheader>
 
       <!-- 환전 요청 -->
-      <v-list-item
-        v-for="(exchangeRequest, idx) in inactiveExchangeRequeests"
-        :key="idx"
-      >
+      <v-list-item v-for="(exchangeRequest, idx) in inactiveExchangeRequeests" :key="idx">
         <v-list-item-avatar>
-          <v-icon
-            class="parent03 lighten-1"
-            disabled
-          >
-            mdi-hand-coin
-          </v-icon>
+          <v-icon class="parent03 lighten-1" disabled> mdi-hand-coin </v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
@@ -142,28 +96,18 @@
 
         <v-list-item-action>
           <v-list-item-action-text v-text="exchangeRequest.requestedTime"></v-list-item-action-text>
-          <bottom-sheet
-            sheetHeight="200px"
-            :isIcon="true"
-            iconName="mdi-information"
-            btnColor="grey lighten-1"
-          >
+          <bottom-sheet sheetHeight="200px" :isIcon="true" iconName="mdi-information" btnColor="grey lighten-1">
             <template v-slot:body>
-              <v-card
-                class="rounded-0"
-                :elevation="0"
-              >
-                <v-toolbar
-                  color="parent01"
-                  dark
-                  :elevation="0"
-                >
+              <v-card class="rounded-0" :elevation="0">
+                <v-toolbar color="parent01" dark :elevation="0">
                   환전 요청 - {{ exchangeRequest.childName }}
                 </v-toolbar>
                 <v-subheader>{{ exchangeRequest.requestedTime }}</v-subheader>
                 <div class="mx-4">
                   <div class="text-h4 font-weight-bold">{{ exchangeRequest.amount }}원</div>
-                  <div class="text-subtitle-1 my-2">{{ exchangeRequest.status === 'approved' ? '환전 완료' : '환전 요청 거절' }}</div>
+                  <div class="text-subtitle-1 my-2">
+                    {{ exchangeRequest.status === "approved" ? "환전 완료" : "환전 요청 거절" }}
+                  </div>
                 </div>
               </v-card>
             </template>
@@ -172,17 +116,9 @@
       </v-list-item>
 
       <!-- 구매 요청 -->
-      <v-list-item
-        v-for="(buyRequest, idx) in inactiveBuyRequests"
-        :key="idx"
-      >
+      <v-list-item v-for="(buyRequest, idx) in inactiveBuyRequests" :key="idx">
         <v-list-item-avatar>
-          <v-icon
-            class="parent03 lighten-1"
-            disabled
-          >
-            mdi-basket
-          </v-icon>
+          <v-icon class="parent03 lighten-1" disabled> mdi-basket </v-icon>
         </v-list-item-avatar>
 
         <v-list-item-content>
@@ -192,27 +128,18 @@
 
         <v-list-item-action>
           <v-list-item-action-text v-text="buyRequest.requestedTime"></v-list-item-action-text>
-          <bottom-sheet
-            :isIcon="true"
-            iconName="mdi-information"
-            btnColor="grey lighten-1"
-          >
+          <bottom-sheet :isIcon="true" iconName="mdi-information" btnColor="grey lighten-1">
             <template v-slot:body>
-              <v-card
-                class="rounded-0"
-                :elevation="0"
-              >
-                <v-toolbar
-                  color="parent02"
-                  class="font-weight-bold"
-                  :elevation="0"
-                >
+              <v-card class="rounded-0" :elevation="0">
+                <v-toolbar color="parent02" class="font-weight-bold" :elevation="0">
                   환전 요청 - {{ buyRequest.childName }}
                 </v-toolbar>
                 <v-subheader>{{ buyRequest.requestedTime }}</v-subheader>
                 <div class="mx-4">
                   <div class="text-h5 font-weight-bold">{{ buyRequest.productName }}</div>
-                  <div class="text-subtitle-1 my-2">{{ buyRequest.status === 'approved' ? '구매 완료' : '구매 요청 거절' }}</div>
+                  <div class="text-subtitle-1 my-2">
+                    {{ buyRequest.status === "approved" ? "구매 완료" : "구매 요청 거절" }}
+                  </div>
                 </div>
               </v-card>
             </template>
@@ -224,77 +151,83 @@
 </template>
 
 <script>
-  import BottomSheet from '@/components/common/BottomSheet'
+import BottomSheet from "@/components/common/BottomSheet";
+import // getRequest,
+//getRequestList,
+// addExchangeRequest,
+// modifyRequestStatusApprove,
+// modifyRequestStatusReject,
+"@/api/requestAPI.js";
 
-  export default {
-    name: 'ParentRequestList',
-    components: {
-      BottomSheet,
+export default {
+  name: "ParentRequestList",
+  components: {
+    BottomSheet,
+  },
+  data() {
+    return {
+      sheetStatus: false,
+      requestList: [],
+      // exchageRequests: [
+      //   {
+      //     childName: "황승연",
+      //     amount: 5000,
+      //     requestedTime: "22.02.05",
+      //     status: "approved",
+      //   },
+      //   {
+      //     childName: "정은이",
+      //     amount: 5000,
+      //     requestedTime: "22.02.05",
+      //     status: "requested",
+      //   },
+      //   {
+      //     childName: "김응철",
+      //     amount: 5000,
+      //     requestedTime: "22.02.05",
+      //     status: "requested",
+      //   },
+      // ],
+      // buyRequests: [
+      //   {
+      //     childName: "우정연",
+      //     productName: "맥북 프로 16인치",
+      //     requestedTime: "22.02.06",
+      //     status: "requested",
+      //   },
+      //   {
+      //     childName: "김지언",
+      //     productName: "맥북 프로 14인치",
+      //     requestedTime: "22.02.06",
+      //     status: "rejected",
+      //   },
+      // ],
+    };
+  },
+  created: {},
+  computed: {
+    activeExchangeRequests() {
+      return this.exchageRequests.filter((request) => {
+        return request.status === "requested";
+      });
     },
-    data() {
-      return {
-        sheetStatus: false,
-        exchageRequests: [
-          {
-            childName: '황승연',
-            amount: 5000,
-            requestedTime: '22.02.05',
-            status: 'approved'
-          },
-          {
-            childName: '정은이',
-            amount: 5000,
-            requestedTime: '22.02.05',
-            status: 'requested'
-          },
-          {
-            childName: '김응철',
-            amount: 5000,
-            requestedTime: '22.02.05',
-            status: 'requested'
-          },
-        ],
-        buyRequests: [
-          {
-            childName: '우정연',
-            productName: '맥북 프로 16인치',
-            requestedTime: '22.02.06',
-            status: 'requested'
-          },
-          {
-            childName: '김지언',
-            productName: '맥북 프로 14인치',
-            requestedTime: '22.02.06',
-            status: 'rejected'
-          }
-        ],
-      }
+    inactiveExchangeRequeests() {
+      return this.exchageRequests.filter((request) => {
+        return request.status !== "requested";
+      });
     },
-    computed: {
-      activeExchangeRequests() {
-        return this.exchageRequests.filter(request => {
-          return request.status === 'requested'
-        })
-      },
-      inactiveExchangeRequeests() {
-        return this.exchageRequests.filter(request => {
-          return request.status !== 'requested'
-        })
-      },
-      activeBuyRequests() {
-        return this.buyRequests.filter(request => {
-          return request.status === 'requested'
-        })
-      },
-      inactiveBuyRequests() {
-        return this.buyRequests.filter(request => {
-          return request.status !== 'requested'
-        })
-      }
-    }
-  }
+    activeBuyRequests() {
+      return this.buyRequests.filter((request) => {
+        return request.status === "requested";
+      });
+    },
+    inactiveBuyRequests() {
+      return this.buyRequests.filter((request) => {
+        return request.status !== "requested";
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
