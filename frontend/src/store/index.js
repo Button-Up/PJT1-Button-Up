7b679ccdde3936b6f821fb4c6f0a6aba67bcd052
rlajiun <rlajiun@kakao.com>
2022-02-09 18:16:10 +0900

    docs: ✏️ index.js에 accountStore 추가

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,20 +1,19 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-import tempAccountStore from './temp/tempAccountStore'
-import userStore from './modules/userStore'
+import tempAccountStore from "./temp/tempAccountStore";
+import userStore from "./modules/userStore";
+import accountStore from "./modules/accountStore";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
     tempAccountStore,
-    userStore
-  }
-})
+    userStore,
+    accountStore,
+  },
+});
