b38590ff4cc52ea1f2dac297212e29233004986f
82surf <82surf@gmail.com>
2022-02-11 02:14:09 +0900

    feat: 🎸 아이 로그인 시 계정 연동 여부 확인
    ✅ Closes: #S06P12B205-289

--- frontend/src/components/child/NotSync.vue
+++ frontend/src/components/child/NotSync.vue
@@ -1,20 +1,35 @@
+<!--
+  author: 유현수
+-->
+
 <template>
-  <v-sheet class="mx-6 d-flex flex-column justify-center" height="500px">
-    <h2>
-      부모님 계정과 연동하면 <br />
-      첫단추를 이용할 수 있어요!
-    </h2>
-    <p>
-      1. 부모님 회원가입을 진행해주세요. <br />
-      2. 자녀 계정 연동 페이지에서 자녀의 아이디를 입력해주세요. <br />
-      3. 부모님 회원가입을 완료하면 계정 연동 완료!
-    </p>
+  <v-sheet class="d-flex align-center" height="500px">
+    <v-sheet class="mx-6 rounded-lg" elevation="2" width="100%">
+      <div class="pa-4">
+        <h2>
+          부모님 계정과 <br />
+          연결이 필요해요!
+        </h2>
+        <p class="mt-3 text-subtitle-2">
+          부모님 회원가입 페이지에서 <br />
+          아래 아이디를 입력하면 계정을 연결할 수 있어요.
+        </p>
+        <v-sheet class="my-3 pa-3 rounded-lg text-center font-weight-bold" color="child01">
+          내 아이디: {{ checkUserInfo.name }}
+        </v-sheet>
+      </div>
+    </v-sheet>
   </v-sheet>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "ChildNotSync",
+  computed: {
+    ...mapGetters("userStore", ["checkUserInfo"]),
+  },
 };
 </script>
 
