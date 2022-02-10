301e9f6fc9e1ad2ecc26912945c029283c5b7b5b
82surf <82surf@gmail.com>
2022-02-10 14:44:47 +0900

    feat: 🎸 로그인 로직 수정 & 회원가입 버튼 생성 & 입력 조건 설정

--- frontend/src/views/common/Login.vue
+++ frontend/src/views/common/Login.vue
@@ -7,19 +7,20 @@
     <div class="mx-9 mt-10">
       <!-- 부모/자녀 탭 -->
       <v-tabs :color="color">
-        <v-tab @click="toggleLoginType">부모님 로그인</v-tab>
-        <v-tab @click="toggleLoginType">자녀 로그인</v-tab>
+        <v-tab @click="loginTypeParent">부모님 로그인</v-tab>
+        <v-tab @click="loginTypeChild">자녀 로그인</v-tab>
       </v-tabs>
 
       <!-- 타이틀 -->
       <h1 class="mt-6">로그인</h1>
 
       <!-- 로그인 폼 -->
-      <v-form>
-        <!-- 휴대폰번호 input -->
+      <v-form v-model="valid">
+        <!-- 아이디 input -->
         <v-text-field
           v-model="credentials.nickname"
-          label="닉네임"
+          :rules="nicknameRules"
+          label="아이디"
           required
           :color="color"
         ></v-text-field>
@@ -27,6 +28,7 @@
         <!-- 비밀번호 input -->
         <v-text-field
           v-model="credentials.password"
+          :rules="passwordRules"
           label="비밀번호"
           required
           :color="color"
@@ -36,13 +38,18 @@
 
         <!-- 로그인 btn -->
         <v-btn
+          :disabled="!valid"
           :color="color"
           block
           :class="{ 'white--text': isParent }"
-          class="font-weight-bold"
+          class="font-weight-bold mt-4"
           @click="login"
         >
           로그인
+        </v-btn>
+        <!-- 회원가입 btn -->
+        <v-btn :color="color" outlined block class="font-weight-bold mt-2">
+          {{ isParent ? "부모님 회원가입" : "자녀 회원가입" }}
         </v-btn>
       </v-form>
     </div>
@@ -52,38 +59,47 @@
 <script>
 import { mapState, mapActions } from "vuex";
 
-
 export default {
   name: "Login",
   data() {
     return {
+      valid: true,
       isParent: true,
       color: "parent01",
       credentials: {
         nickname: null,
         password: null,
       },
+      nicknameRules: [(v) => !!v || "아이디를 입력해주세요."],
+      passwordRules: [(v) => !!v || "비밀번호를 입력해주세요."],
     };
   },
   computed: {
-    ...mapState('userStore', ['isLogin', 'isLoginError']),
+    ...mapState("userStore", ["isLogin", "isLoginError"]),
   },
   methods: {
-    ...mapActions('userStore', ['vuexLogin', 'vuexGetUserInfo']),
+    ...mapActions("userStore", ["vuexLogin", "vuexGetUserInfo"]),
 
     async login() {
       const loginInfo = {
         isParent: this.isParent,
-        credentials: this.credentials
-      }
+        credentials: this.credentials,
+      };
       await this.vuexLogin(loginInfo);
-      await this.vuexGetUserInfo(loginInfo);
-      this.$router.push(this.isParent ? '/parent/home' : '/child/home')
+      if (this.isLogin) {
+        await this.vuexGetUserInfo(loginInfo);
+        this.$router.push(this.isParent ? "/parent/home" : "/child/home");
+      } else {
+        alert("아이디 혹은 비밀번호가 맞지 않습니다.");
+      }
     },
-
-    toggleLoginType() {
-      this.isParent = !this.isParent;
-      this.color = this.isParent ? "parent01" : "child01";
+    loginTypeParent() {
+      this.isParent = true;
+      this.color = "parent01";
+    },
+    loginTypeChild() {
+      this.isParent = false;
+      this.color = "child01";
     },
   },
 };
