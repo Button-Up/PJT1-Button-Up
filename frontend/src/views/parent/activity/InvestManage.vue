d6820902d8298aa8dd807c6248b9fd0886a55417
EuneeChung <chungee375@gmail.com>
2022-02-15 01:04:09 +0900

    feat: 🎸 [Frontend] 투자 활동 - 투자 종목 레이아웃
    ✅ Closes: #S06P12B205-346

--- frontend/src/views/parent/activity/InvestManage.vue
+++ frontend/src/views/parent/activity/InvestManage.vue
@@ -1,20 +1,36 @@
 <template>
-  <div>
+  <div class="mx-6 mt-6">
     <!-- 투자 종목 리스트 -->
-    <div class="mt-2">
-      <h2>투자 종목</h2>
+    <div class="mb-4">
+      <v-row class="ma-0 align-center justify-space-between">
+        <h2>투자 종목</h2>
+        <v-btn
+          small
+          :color="`parent01`"
+          class="white--text"
+          @click="$router.push('/parent/activity/job')"
+          >추가하기</v-btn
+        >
+      </v-row>
     </div>
-    <v-list-item v-for="(todo, t) in TodoList" :key="t" class="mb-2 pa-2" style="display: contents">
+
+    <v-list-item
+      v-for="(todo, t) in convertToFitToDoComponents"
+      :key="t"
+      class="mb-2 pa-2"
+      style="display: contents"
+    >
       <TodoList :todo="todo" :isParent="true"></TodoList>
     </v-list-item>
   </div>
 </template>
 
 <script>
-import TodoList from "../../../components/common/TodoList.vue";
+import TodoList from '../../../components/common/TodoList.vue';
+import { mapGetters } from 'vuex';
 
 export default {
-  name: "InvestManage",
+  name: 'InvestManage',
   components: {
     TodoList,
   },
@@ -22,22 +38,45 @@ export default {
     return {
       TodoList: [
         {
-          done: false,
-          content: "환전/결제 요청 확인",
-          url: "/parent/request-list",
+          done: true,
+          content: '환전/결제 요청 확인',
+          url: '/parent/request-list',
         },
         {
           done: true,
-          content: "투자 가격 업데이트",
-          url: "/parent/userinfo",
+          content: '투자 가격 업데이트',
+          url: '/parent/userinfo',
         },
         {
           done: true,
-          content: "투자 가격 업데이트",
-          url: "/parent/userinfo",
+          content: '투자 가격 업데이트',
+          url: '/parent/userinfo',
         },
       ],
     };
+  },
+  mounted() {
+    this.getInvest();
+  },
+  computed: {
+    ...mapGetters('investStore', ['getInvestList']),
+    ...mapGetters('userStore', ['checkUserInfo']),
+    convertToFitToDoComponents() {
+      var convertInvest = [];
+      for (const investItem of this.getInvestList) {
+        convertInvest.push({
+          done: true,
+          content: investItem.name,
+          url: '/parent/activity/job',
+        });
+      }
+      return convertInvest;
+    },
+  },
+  methods: {
+    getInvest() {
+      this.$store.dispatch('investStore/vuexUpdateInvestList', this.checkUserInfo.seq);
+    },
   },
 };
 </script>
