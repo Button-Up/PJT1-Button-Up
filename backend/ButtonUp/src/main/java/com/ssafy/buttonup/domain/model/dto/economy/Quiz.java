70da696055816af7be398b663de7ac66a8861fc8
EuneeChung <chungee375@gmail.com>
2022-02-14 11:50:24 +0900

    fix: 🐛 [Backend] Table명 s 추가

---
+++ backend/ButtonUp/src/main/java/com/ssafy/buttonup/domain/model/dto/economy/Quiz.java
@@ -0,0 +1,2 @@
+package com.ssafy.buttonup.domain.model.dto.economy;public class Quiz {
+}
---
+++ backend/ButtonUp/src/main/java/com/ssafy/buttonup/domain/model/dto/economy/response/QuizResponse.java
@@ -0,0 +1,2 @@
+package com.ssafy.buttonup.domain.model.dto.economy.response;public class QuizResponse {
+}
