b9d0b1f711c9699ec97eaf394c6503a6e3a75922
EuneeChung <chungee375@gmail.com>
2022-02-14 21:04:45 +0900

    feat: 🎸 [Backend] 경제주제별 상세 페이지 조회
    ✅ Closes: #S06P12B205-335

--- backend/ButtonUp/src/main/java/com/ssafy/buttonup/controller/economy/EconomyController.java
+++ backend/ButtonUp/src/main/java/com/ssafy/buttonup/controller/economy/EconomyController.java
@@ -1,13 +1,17 @@
 package com.ssafy.buttonup.controller.economy;
 
+import com.ssafy.buttonup.domain.model.dto.economy.response.PostResponse;
 import com.ssafy.buttonup.domain.model.dto.invest.response.NewsResponse;
+import com.ssafy.buttonup.domain.model.entity.economy.EconomyPost;
 import com.ssafy.buttonup.domain.service.economy.EconomyService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +34,12 @@ public class EconomyController {
     @ApiOperation(value = "경제 주제 조회", notes = "")
     public ResponseEntity<List<String>> getEconomyTopic(){
         return new ResponseEntity<>(economyService.getEconomyTopic(), HttpStatus.OK);
+    }
+
+    @GetMapping("economy-posts/{topic_name}")
+    @ApiOperation(value = "경제 주제별 내용 조회")
+    public ResponseEntity<List<PostResponse>> getContentOfTopic(@ApiParam(value = "경제 주제 이름",required = true)@PathVariable("topic_name") String topicName){
+        return new ResponseEntity<>(economyService.getEconomyPost(topicName),HttpStatus.OK);
+
     }
 }
