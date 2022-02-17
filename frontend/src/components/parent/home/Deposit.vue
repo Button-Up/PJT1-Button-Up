6a39e4617bd8e3c840e8c4098e4897d9b66dc36b
82surf <82surf@gmail.com>
2022-02-17 17:19:07 +0900

    feat: 🎸 부모 아이 상세정보에서 입금 후 모달 닫히도록 수정

--- frontend/src/components/parent/home/Deposit.vue
+++ frontend/src/components/parent/home/Deposit.vue
@@ -4,16 +4,27 @@
   modified : 우정연 - 입금시 type, content 추가
 -->
 <template>
-  <v-card class="rounded-0" :elevation="0">
-    <v-toolbar color="parent02" class="font-weight-bold" :elevation="0"> 입금하기 </v-toolbar>
-    <v-subheader>입금할 단추 개수를 입력해주세요.</v-subheader>
-    <v-form class="mx-4">
-      <v-text-field label="단추" suffix="개" color="parent01" v-model="money"></v-text-field>
-    </v-form>
-    <v-card-actions>
-      <v-btn block color="parent01" outlined rounded text @click="addAccount"> 입금 </v-btn>
-    </v-card-actions>
-  </v-card>
+  <div>
+    <v-card class="rounded-0" :elevation="0">
+      <v-toolbar color="parent01" class="white--text" :elevation="0"> 입금하기 </v-toolbar>
+      <div class="pa-3">
+        <v-subheader>입금할 단추 개수를 입력해주세요.</v-subheader>
+        <v-form class="mx-4">
+          <v-text-field label="단추" suffix="개" color="parent01" v-model="money"></v-text-field>
+        </v-form>
+        <v-card-actions>
+          <v-btn
+            block
+            color="parent01"
+            @click="[addAccount(), $emit('clickDeposit')]"
+            class="white--text"
+          >
+            입금
+          </v-btn>
+        </v-card-actions>
+      </div>
+    </v-card>
+  </div>
 </template>
 
 <script>
